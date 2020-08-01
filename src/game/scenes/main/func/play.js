195bb65dccc136b880ab6ea2e44acf95b35e3f73
kayacchang <egg734631@gmail.com>
2020-08-01 17:52:03 +0800

    Fix

--- src/game/scenes/main/func/play.js
+++ src/game/scenes/main/func/play.js
@@ -1,200 +1,187 @@
-import {spin} from './spin'
-import {wait, clone, times, ceil, log, table} from '@kayac/utils'
-import anime from 'animejs'
+import { spin } from "./spin";
+import { wait, clone, times, ceil, log, table } from "@kayac/utils";
+import anime from "animejs";
 import {
-    bigWinEffect,
-    freeGameEffect,
-    reSpinEffect,
-    scoresEffect,
-} from '../components/effects'
-import {Coin, playCoin} from '../components/coin'
+  bigWinEffect,
+  freeGameEffect,
+  reSpinEffect,
+  scoresEffect,
+} from "../components/effects";
+import { Coin, playCoin } from "../components/coin";
 
-export function play (scene) {
-    const {slot, energy, neko, freeSpinIcon, normalTable, freeGameTable} = scene
+export function play(scene) {
+  const {
+    slot,
+    energy,
+    neko,
+    freeSpinIcon,
+    normalTable,
+    freeGameTable,
+    respinTable,
+  } = scene;
 
-    const coinPos = slot.view.children.filter(({name}) => name.includes('Coin'))
+  const coinPos = slot.view.children.filter(({ name }) =>
+    name.includes("Coin")
+  );
 
-    app.on('GameResult', async (result) => {
-        log('Result =============')
-        table(result)
+  app.on("GameResult", async (result) => {
+    log("Result =============");
+    table(result);
 
-        const normalWin = result.normalGame.scores
-        const reSpinWin = result.hasReSpin ? result.reSpinGame.scores : 0
-        const freeWin = result.hasFreeGame
-            ? result.freeGame.reduce((a, b) => a.scores + b.scores)
-            : 0
-        const totalWin = normalWin + reSpinWin + freeWin
+    const normalWin = result.normalGame.scores;
+    const reSpinWin = result.hasReSpin ? result.reSpinGame.scores : 0;
+    const freeWin = result.hasFreeGame
+      ? result.freeGame.reduce((a, b) => a.scores + b.scores)
+      : 0;
+    const totalWin = normalWin + reSpinWin + freeWin;
 
-        await spin(scene, slot.reels, result.normalGame)
+    await spin(scene, slot.reels, result.normalGame);
 
-        if (result.earnPoints !== energy.scale) {
-            await energy.update(result.earnPoints)
-        }
-
-        if (result.normalGame.scores && !result.hasReSpin) {
-            await showScores(normalWin)
-            await showBigWinEffect(normalWin)
-
-            app.user.cash += normalWin
-            app.user.lastWin = normalWin
-        }
-
-        if (result.hasReSpin) {
-            const reSpinTable = clone(normalTable)
-
-            reSpinTable[1] = [
-                2,
-                4,
-                3,
-                2,
-                3,
-                2,
-                3,
-                4,
-                2,
-                3,
-                2,
-                2,
-                3,
-                2,
-                2,
-                3,
-                4,
-                2,
-                3,
-                2,
-            ]
-
-            slot.reelTables = reSpinTable
-            await wait(1000)
-            await neko.appear()
-            await wait(1000)
-            neko.hit()
-
-            await wait(150)
-
-            scene.y = 10
-            anime({
-                targets: scene,
-                y: 0,
-                easing: 'easeOutElastic(10, .1)',
-                duration: 750,
-            })
-
-            slot.view.getChildByName('FXReel_M').anim.visible = true
-
-            app.sound.play('maybeBonus')
-
-            reSpinEffect(scene)
-
-            await spin(scene, [slot.reels[1]], result.reSpinGame)
-
-            await showScores(reSpinWin)
-
-            await showBigWinEffect(reSpinWin)
-
-            app.user.cash += reSpinWin
-            app.user.lastWin = reSpinWin
-        }
-
-        if (energy.scale === 10) {
-            freeSpinIcon.shock()
-            await wait(2000)
-            await neko.appear()
-            await wait(1000)
-            neko.hit()
-
-            await wait(150)
-
-            scene.y = 10
-            anime({
-                targets: scene,
-                y: 0,
-                easing: 'easeOutElastic(10, .1)',
-                duration: 750,
-            })
-
-            freeSpinIcon.stop()
-
-            slot.reelTables = freeGameTable
-
-            const freeGame = result.freeGame
-            let totalScores = 0
-            for (const result of freeGame) {
-                table(result)
-                const multiply = freeGame.indexOf(result) + 1
-
-                freeGameEffect(scene, multiply)
-
-                slot.view.children
-                    .filter(
-                        ({name}) => name === 'FXReel_L' || name === 'FXReel_R',
-                    )
-                    .forEach(({anim}) => (anim.visible = true))
-
-                app.sound.play('maybeBonus')
-
-                await spin(
-                    scene,
-                    slot.reels.filter(({reelIdx}) => reelIdx !== 1),
-                    result,
-                )
-
-                totalScores += result.scores
-                if (result.scores) {
-                    await showScores(result.scores)
-                }
-            }
-            await showBigWinEffect(totalScores)
-
-            app.user.cash += totalScores
-            app.user.lastWin = totalScores
-
-            await energy.update(0)
-        }
-
-        app.user.cash = result.cash
-        app.user.totalWin += totalWin
-
-        slot.reelTables = normalTable
-
-        betLock(result.hasBetLock)
-
-        if (totalWin > 0) await wait(750)
-
-        log('Round Complete...')
-        app.emit('Idle')
-    })
-
-    function betLock (flag) {
-        app.user.isBetLock = flag
+    if (result.earnPoints !== energy.scale) {
+      await energy.update(result.earnPoints);
     }
 
-    async function showScores (scores) {
-        const odds = ceil(scores / app.user.betOptions[app.user.bet])
+    if (result.normalGame.scores && !result.hasReSpin) {
+      await showScores(normalWin);
+      await showBigWinEffect(normalWin);
 
-        const coinEffect = slot.reels.map((reel) => {
-            const coins = times(Coin, odds)
-
-            const pos = coinPos[reel.reelIdx]
-
-            if (coins.length <= 0) {
-                return Promise.resolve()
-            }
-
-            return playCoin(scene, pos, coins)
-        })
-
-        const effect = scoresEffect(scene, scores)
-
-        await Promise.all([...coinEffect, effect])
+      app.user.cash += normalWin;
+      app.user.lastWin = normalWin;
     }
 
-    async function showBigWinEffect (scores) {
-        const odds = scores / app.user.betOptions[app.user.bet]
+    if (result.hasReSpin) {
+      const reSpinTable = clone(normalTable);
 
-        if (odds <= 8) return
+      reSpinTable[1] = respinTable[0];
 
-        await bigWinEffect(scene, scores)
+      slot.reelTables = reSpinTable;
+      await wait(1000);
+      await neko.appear();
+      await wait(1000);
+      neko.hit();
+
+      await wait(150);
+
+      scene.y = 10;
+      anime({
+        targets: scene,
+        y: 0,
+        easing: "easeOutElastic(10, .1)",
+        duration: 750,
+      });
+
+      slot.view.getChildByName("FXReel_M").anim.visible = true;
+
+      app.sound.play("maybeBonus");
+
+      reSpinEffect(scene);
+
+      await spin(scene, [slot.reels[1]], result.reSpinGame);
+
+      await showScores(reSpinWin);
+
+      await showBigWinEffect(reSpinWin);
+
+      app.user.cash += reSpinWin;
+      app.user.lastWin = reSpinWin;
     }
+
+    if (energy.scale === 10) {
+      freeSpinIcon.shock();
+      await wait(2000);
+      await neko.appear();
+      await wait(1000);
+      neko.hit();
+
+      await wait(150);
+
+      scene.y = 10;
+      anime({
+        targets: scene,
+        y: 0,
+        easing: "easeOutElastic(10, .1)",
+        duration: 750,
+      });
+
+      freeSpinIcon.stop();
+
+      slot.reelTables = freeGameTable;
+
+      const freeGame = result.freeGame;
+      let totalScores = 0;
+      for (const result of freeGame) {
+        table(result);
+        const multiply = freeGame.indexOf(result) + 1;
+
+        freeGameEffect(scene, multiply);
+
+        slot.view.children
+          .filter(({ name }) => name === "FXReel_L" || name === "FXReel_R")
+          .forEach(({ anim }) => (anim.visible = true));
+
+        app.sound.play("maybeBonus");
+
+        await spin(
+          scene,
+          slot.reels.filter(({ reelIdx }) => reelIdx !== 1),
+          result
+        );
+
+        totalScores += result.scores;
+        if (result.scores) {
+          await showScores(result.scores);
+        }
+      }
+      await showBigWinEffect(totalScores);
+
+      app.user.cash += totalScores;
+      app.user.lastWin = totalScores;
+
+      await energy.update(0);
+    }
+
+    app.user.cash = result.cash;
+    app.user.totalWin += totalWin;
+
+    slot.reelTables = normalTable;
+
+    betLock(result.hasBetLock);
+
+    if (totalWin > 0) await wait(750);
+
+    log("Round Complete...");
+    app.emit("Idle");
+  });
+
+  function betLock(flag) {
+    app.user.isBetLock = flag;
+  }
+
+  async function showScores(scores) {
+    const odds = ceil(scores / app.user.betOptions[app.user.bet]);
+
+    const coinEffect = slot.reels.map((reel) => {
+      const coins = times(Coin, odds);
+
+      const pos = coinPos[reel.reelIdx];
+
+      if (coins.length <= 0) {
+        return Promise.resolve();
+      }
+
+      return playCoin(scene, pos, coins);
+    });
+
+    const effect = scoresEffect(scene, scores);
+
+    await Promise.all([...coinEffect, effect]);
+  }
+
+  async function showBigWinEffect(scores) {
+    const odds = scores / app.user.betOptions[app.user.bet];
+
+    if (odds <= 8) return;
+
+    await bigWinEffect(scene, scores);
+  }
 }
--- src/game/scenes/main/scene.js
+++ src/game/scenes/main/scene.js
@@ -1,99 +1,100 @@
-import {addPackage} from 'pixi_fairygui'
+import { addPackage } from "pixi_fairygui";
 
-import * as data from './data'
+import * as data from "./data";
 
-import {SlotMachine} from './components/slot'
-import {Neko} from './components/neko'
-import {FreeSpinIcon} from './components/freespin'
-import {EnergyBar} from './components/energy'
+import { SlotMachine } from "./components/slot";
+import { Neko } from "./components/neko";
+import { FreeSpinIcon } from "./components/freespin";
+import { EnergyBar } from "./components/energy";
 
-import {setBevel, setGlow} from '../../plugin/filter'
+import { setBevel, setGlow } from "../../plugin/filter";
 
-import {play} from './func/play'
+import { play } from "./func/play";
 
-function initSlotMachine (scene, reelTables) {
-    const slot = SlotMachine({
-        view: scene.getChildByName('SlotMachine'),
-        reelTables,
-        ...data,
+function initSlotMachine(scene, reelTables) {
+  const slot = SlotMachine({
+    view: scene.getChildByName("SlotMachine"),
+    reelTables,
+    ...data,
+  });
+
+  setBevel(slot.view, { thickness: 5 });
+
+  const title = slot.view.getChildByName("Title");
+
+  setBevel(title);
+
+  const tasks = slot.view.children
+    .map((target) => {
+      if (target.name.includes("FXReel")) {
+        setGlow(target, {
+          innerStrength: 1,
+          outerStrength: 1,
+          distance: 6,
+          color: 0xfcffa3,
+        });
+
+        return whenAnimComplete(target);
+      } else if (target.name.includes("Effect")) {
+        return target.children.map(whenAnimComplete);
+      }
+      return Promise.resolve();
     })
+    .flat();
 
-    setBevel(slot.view, {thickness: 5})
+  Promise.all(tasks).then(() => slot.view.emit("Ready"));
 
-    const title = slot.view.getChildByName('Title')
+  return slot;
 
-    setBevel(title)
+  function whenAnimComplete(fx) {
+    fx.anim.gotoAndPlay(0);
 
-    const tasks = slot.view.children
-        .map((target) => {
-            if (target.name.includes('FXReel')) {
-                setGlow(target, {
-                    innerStrength: 1,
-                    outerStrength: 1,
-                    distance: 6,
-                    color: 0xfcffa3,
-                })
-
-                return whenAnimComplete(target)
-            } else if (target.name.includes('Effect')) {
-                return target.children.map(whenAnimComplete)
-            }
-            return Promise.resolve()
-        })
-        .flat()
-
-    Promise.all(tasks).then(() => slot.view.emit('Ready'))
-
-    return slot
-
-    function whenAnimComplete (fx) {
-        fx.anim.gotoAndPlay(0)
-
-        return new Promise((resolve) =>
-            fx.anim.once('complete', function onComplete () {
-                fx.anim.visible = false
-                fx.anim.off('complete', onComplete)
-                resolve()
-            }),
-        )
-    }
+    return new Promise((resolve) =>
+      fx.anim.once("complete", function onComplete() {
+        fx.anim.visible = false;
+        fx.anim.off("complete", onComplete);
+        resolve();
+      })
+    );
+  }
 }
 
-export function create (app, {normalTable, freeGameTable}) {
-    const create = addPackage(app, 'main')
-    const scene = create('MainScene')
+export function create(app, { normalTable, freeTable, respinTable }) {
+  const create = addPackage(app, "main");
+  const scene = create("MainScene");
 
-    const slot = initSlotMachine(scene, normalTable)
+  const slot = initSlotMachine(scene, normalTable);
 
-    const energy = EnergyBar(slot.view.getChildByName('EnergyBar'))
+  const energy = EnergyBar(slot.view.getChildByName("EnergyBar"));
 
-    const freeSpinIcon = FreeSpinIcon(slot.view.getChildByName('Icon@freespin'))
+  const freeSpinIcon = FreeSpinIcon(slot.view.getChildByName("Icon@freespin"));
 
-    const neko = Neko(scene)
+  const neko = Neko(scene);
 
-    scene.slot = slot
-    scene.energy = energy
-    scene.neko = neko
-    scene.freeSpinIcon = freeSpinIcon
-    scene.normalTable = normalTable
-    scene.freeGameTable = freeGameTable
+  scene.slot = slot;
+  scene.energy = energy;
+  scene.neko = neko;
+  scene.freeSpinIcon = freeSpinIcon;
+  scene.normalTable = normalTable;
+  scene.freeGameTable = freeTable;
+  scene.respinTable = respinTable;
 
-    play(scene)
+  play(scene);
 
-    slot.view.once('Ready', () => {
-        app.emit('GameReady')
+  slot.view.once("Ready", () => {
+    app.emit("GameReady");
 
-        const loadScene = app.stage.getChildByName('LoadScene')
-        app.stage.removeChild(loadScene)
-    })
+    const loadScene = app.stage.getChildByName("LoadScene");
+    app.stage.removeChild(loadScene);
+  });
 
-    app.alert
-        .request({title: app.translate(`common:message.audio`)})
-        .then(({value}) => {
-            app.sound.mute(!value)
+  app.alert
+    .request({ title: app.translate(`common:message.audio`) })
+    .then(({ value }) => {
+      app.sound.mute(!value);
 
-            app.sound.play('mainBGM')
-        })
+      app.sound.play("mainBGM");
+    });
 
-    return scene
+  return scene;
 }
